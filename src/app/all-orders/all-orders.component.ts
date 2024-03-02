7f208cd8b2de4f51bb65243ebebbd217cb6284ca
Mohamed Khalel <mn911415@gmail.com>
2024-03-02 14:56:03 +0200

    'upload'

---
+++ src/app/all-orders/all-orders.component.ts
@@ -0,0 +1,34 @@
+import { CartItem, ShippingAddress } from './../order';
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../order.service';
+import { AuthService } from '../auth.service';
+import { Order } from '../order';
+
+@Component({
+  selector: 'app-all-orders',
+  templateUrl: './all-orders.component.html',
+  styleUrls: ['./all-orders.component.scss']
+})
+export class AllOrdersComponent implements OnInit {
+  UserId:any
+  orderDetails:Order[] =[]
+  carItems:any
+  constructor(private _OrderService:OrderService,private _AuthService:AuthService){}
+  ngOnInit(): void {
+    this._AuthService.saveUserData()
+    this.UserId=this._AuthService.userDataVar.getValue().id
+    console.log(this.UserId);
+    this._OrderService.getUserOrders(this.UserId).subscribe({
+      next:data=>{
+        this.orderDetails=data
+        console.log(this.UserId);
+        console.log(data);
+      },
+      error:err=>{
+        console.log(err);
+        
+      }
+    })
+  }
+
+}
